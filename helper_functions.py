347c971ebe11b3ac57598f7de08b675097dd5476
notcis <notcis07@gmail.com>
2026-03-14 17:42:32 +0700

    gpu info

--- helper_functions.py
+++ helper_functions.py
@@ -7,8 +7,6 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 
-from torch import nn
-
 import os
 import zipfile
 
@@ -292,3 +290,32 @@ def download_data(source: str,
             os.remove(data_path / target_file)
     
     return image_path
+
+
+def gpu_info():
+    if torch.cuda.is_available():
+        gpu_info = !nvidia-smi
+        gpu_info = '\n'.join(gpu_info)
+        if gpu_info.find("failed") >= 0:
+            print("Not connected to a GPU, to leverage the best of PyTorch 2.0, you should connect to a GPU.")
+
+        # Get GPU name
+        gpu_name = !nvidia-smi --query-gpu=gpu_name --format=csv
+        gpu_name = gpu_name[1]
+        GPU_NAME = gpu_name.replace(" ", "_") # remove underscores for easier saving
+        print(f'GPU name: {GPU_NAME}')
+
+        # Get GPU capability score
+        GPU_SCORE = torch.cuda.get_device_capability()
+        print(f"GPU capability score: {GPU_SCORE}")
+        if GPU_SCORE >= (8, 0):
+            print(f"GPU score higher than or equal to (8, 0), PyTorch 2.x speedup features available.")
+        else:
+            print(f"GPU score lower than (8, 0), PyTorch 2.x speedup features will be limited (PyTorch 2.x speedups happen most on newer GPUs).")
+        
+        # Print GPU info
+        print(f"GPU information:\n{gpu_info}")
+
+    else:
+        print("PyTorch couldn't find a GPU, to leverage the best of PyTorch 2.0, you should connect to a GPU.")
+    

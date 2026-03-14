2dda69f0e6c8e20e9b19a7c5dbcdc1130aafe3aa
notcis <notcis07@gmail.com>
2026-03-14 17:54:47 +0700

    fix

--- helper_functions.py
+++ helper_functions.py
@@ -291,31 +291,4 @@ def download_data(source: str,
     
     return image_path
 
-
-def gpu_info():
-    if torch.cuda.is_available():
-        gpu_info = !nvidia-smi
-        gpu_info = '\n'.join(gpu_info)
-        if gpu_info.find("failed") >= 0:
-            print("Not connected to a GPU, to leverage the best of PyTorch 2.0, you should connect to a GPU.")
-
-        # Get GPU name
-        gpu_name = !nvidia-smi --query-gpu=gpu_name --format=csv
-        gpu_name = gpu_name[1]
-        GPU_NAME = gpu_name.replace(" ", "_") # remove underscores for easier saving
-        print(f'GPU name: {GPU_NAME}')
-
-        # Get GPU capability score
-        GPU_SCORE = torch.cuda.get_device_capability()
-        print(f"GPU capability score: {GPU_SCORE}")
-        if GPU_SCORE >= (8, 0):
-            print(f"GPU score higher than or equal to (8, 0), PyTorch 2.x speedup features available.")
-        else:
-            print(f"GPU score lower than (8, 0), PyTorch 2.x speedup features will be limited (PyTorch 2.x speedups happen most on newer GPUs).")
-        
-        # Print GPU info
-        print(f"GPU information:\n{gpu_info}")
-
-    else:
-        print("PyTorch couldn't find a GPU, to leverage the best of PyTorch 2.0, you should connect to a GPU.")
     
